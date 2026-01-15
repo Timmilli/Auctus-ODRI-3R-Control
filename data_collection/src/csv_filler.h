ae1aa0ca423e1e51f1e915b0de75c9209eab2832
Guenael Roger <guenael.mail@gmail.com>
2026-01-15 18:03:23 +0100

    new class to deal with csv

--- data_collection/src/csv_filler.h
+++ data_collection/src/csv_filler.h
@@ -1,12 +1,23 @@
+#pragma once
+
+#include <fstream>
+
 class CsvFiller {
 private:
   char _filename[32];
+  std::ofstream _fd;
 
 public:
   CsvFiller();
   CsvFiller(char filename[32]);
-  CsvFiller(char filename[32], char *header);
   ~CsvFiller();
+  /* TODO switch to a dynamic filename
+   * CsvFiller();
+   * CsvFiller(char *filename);
+   */
 
-  void Write(char *data);
+  void openFile();
+  void closeFile();
+  void writeString(char *str);
+  void writeData(double time, double err);
 };
