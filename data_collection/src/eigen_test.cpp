330914918d45e0f7a0c3a5a74356e56742702107
Guenael Roger <guenael.mail@gmail.com>
2026-01-16 17:24:24 +0100

    add reading through csv to transform each point

--- data_collection/src/eigen_test.cpp
+++ data_collection/src/eigen_test.cpp
@@ -1,26 +1,59 @@
 #include <Eigen/Dense>
 #include <cmath>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 #include "mgi.h"
 
 using Eigen::Vector3d;
 
 int main() {
-  int max_iter = 10;
+  Vector3d abc(0.5, 0.5, 0.5);
+  std::cout << J_pinv(abc) << std::endl;
 
-  Vector3d Xd(0., 0.450, 0.);
+  return 0;
+
+  std::ifstream file("trajectory.csv");
+  if (!file.is_open()) {
+    std::cerr << "Failed to open CSV file\n";
+    return 1;
+  }
+
+  std::vector<Eigen::Vector3d> points;
+  std::string line;
+
+  while (std::getline(file, line)) {
+    std::stringstream ss(line);
+    std::string value;
+
+    Eigen::Vector3d vec;
+    for (int i = 0; i < 3; ++i) {
+      if (!std::getline(ss, value, ',')) {
+        std::cerr << "Invalid row format\n";
+        return 1;
+      }
+      vec(i) = std::stod(value);
+    }
+
+    points.push_back(vec);
+  }
+
+  file.close();
+
   Vector3d q(0.1, 0.1, 0.1);
 
-  std::cout << "Basic q:\n" << q << std::endl;
+  int max_iter = 1;
 
-  for (int i = 0; i < max_iter; i++) {
-    gradient_descent(q, Xd);
-    std::cout << "middle q:\n" << q << std::endl;
+  // Example usage
+  for (Vector3d &Xd : points) {
+    std::cout << "\nConfiguration:\n" << q << std::endl;
+
+    for (int i = 0; i < max_iter; i++) {
+      gradient_descent(q, Xd);
+    }
   }
-  std::cout << "Final q:\n" << q << std::endl;
-
-  q = q * 360 / (120 * M_PI);
 
   std::cout << "Most Final q:\n" << q << std::endl;
 
