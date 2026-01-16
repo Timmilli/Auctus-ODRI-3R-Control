f56dc6ef368de1aec234be4538f92bf598ba8b61
Guenael Roger <guenael.mail@gmail.com>
2026-01-16 15:56:42 +0100

    split eigen test and mgi implementation

---
+++ data_collection/src/mgi.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+using Eigen::Vector3d;
+
+#define L2 0.160
+#define L3 0.160
+#define H1 0.240
+#define H2 0.020
+#define H3 0.020
+
+/*
+ * @brief Does a gradient descent once such as
+ * q = q - J_pinv*F
+ */
+void gradient_descent(Vector3d &q, Vector3d &Xd);
